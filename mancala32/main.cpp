51dd8d5a55cb23c7c59953f346ceffc3657149c7
aurelius <davids94@comcast.net>
2018-02-15 15:05:36 -0800

    Initial Commit

---
+++ mancala32/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mancala32
+//
+//  Created by David Sanville on 2/15/18.
+//  Copyright © 2018 David Sanville. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
